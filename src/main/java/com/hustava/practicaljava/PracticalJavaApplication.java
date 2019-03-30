91e3c15619e5301322f6b7892cc60fdd23abf4e3
Húšťava Dominik <dhustava@trask.cz>
2019-03-30 13:06:38 +0100

    Initial Commit

---
+++ src/main/java/com/hustava/practicaljava/PracticalJavaApplication.java
@@ -0,0 +1,16 @@
+package com.hustava.practicaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PracticalJavaApplication{
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalJavaApplication.class, args);
+	}
+
+}
